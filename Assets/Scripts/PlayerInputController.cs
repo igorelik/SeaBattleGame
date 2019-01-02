cfbc8df0b547309eca199669060b84acdcd1415d
Igor Gorelik <gorelik.igor@gmail.com>
2019-01-02 15:57:39 +1100

    Initial commit

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var horRotation = Input.GetAxis("Horizontal");
+        //Debug.Log($"Hor rotation = {horRotation}");
+        this.gameObject.transform.Rotate(new Vector3(0, 1, 0), horRotation);
+    }
+}
