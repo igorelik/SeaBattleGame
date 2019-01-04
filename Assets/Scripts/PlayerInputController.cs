63632b8d2c26345669f39a05a50fcc862506d6ee
Igor Gorelik <gorelik.igor@gmail.com>
2019-01-04 11:09:22 +0000

    Merged PR 2: Moving torpedo
    Moving torpedo
    
    Related work items: #63

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,12 +1,41 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerInputController : MonoBehaviour
 {
+    private GameObject _torpedo;
+    private TorpedoController _torpedoController;
+
+    private Vector3 _originalTorpedoPosition;
+
+    void Start()
+    {
+        _torpedo = GameObject.FindGameObjectWithTag("Torpedo");
+        _torpedoController = _torpedo.GetComponent<TorpedoController>();
+        _originalTorpedoPosition = _torpedo.transform.position;
+        //_torpedo.SetActive(false);
+        var vb = new CrossPlatformInputManager.VirtualButton("Fire1");
+        CrossPlatformInputManager.RegisterVirtualButton(vb);
+    }
+    
     // Update is called once per frame
     void Update()
     {
         var horRotation = Input.GetAxis("Horizontal");
         //Debug.Log($"Hor rotation = {horRotation}");
         this.gameObject.transform.Rotate(new Vector3(0, 1, 0), horRotation);
+        if (!_torpedoController.IsMoving)
+        {
+            _torpedo.transform.Rotate(new Vector3(0, 1, 0), horRotation);
+        }
+
+        if (!_torpedoController.IsMoving && CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            Debug.Log("FIRE");
+            //_torpedo.SetActive(true);
+            _torpedo.transform.position = _originalTorpedoPosition;
+           // _torpedo.transform.rotation = this.gameObject.transform.rotation;
+            _torpedoController.IsMoving = true;
+        }
     }
 }
---
+++ Assets/Scripts/TorpedoController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorpedoController : MonoBehaviour
+{
+    public float Speed = 1f;
+    public bool IsMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+//        if (this.gameObject.activeSelf)
+        if (IsMoving)
+        {
+            this.gameObject.transform.position += this.gameObject.transform.right * Speed;
+        }
+    }
+}
