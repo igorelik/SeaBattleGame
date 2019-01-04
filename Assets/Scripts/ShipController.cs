babb2f858a4400bdf88279f872c98461920cfce4
Igor Gorelik <gorelik.igor@gmail.com>
2019-01-04 02:48:24 +0000

    Merged PR 1: Add moving ship
    Related work items: #64

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float Speed = 1f;
+    // Start is called before the first frame update
+    private Vector3 _originalPosition;
+    private Quaternion _originalRotation;
+    void Start()
+    {
+        _originalPosition = this.gameObject.transform.position;
+        _originalRotation = this.gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(Speed, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Scene")
+        {
+            Debug.Log($"End of the world. Turning");
+            Speed *= -1;
+            return;
+        }
+    }
+
+}
