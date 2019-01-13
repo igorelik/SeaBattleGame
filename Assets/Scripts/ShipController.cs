6100db0ee341dcc0b86ae412e5145aaa9e6d428b
Igor Gorelik <gorelik.igor@gmail.com>
2019-01-13 14:21:12 +1100

    Sinking ships

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class ShipController : MonoBehaviour
     private Vector3 _originalPosition;
     private Quaternion _originalRotation;
     private int _hitCount = 0;
+    private List<Transform> _explosions = new List<Transform>();
+
     void Start()
     {
         _originalPosition = this.gameObject.transform.position;
@@ -23,23 +27,45 @@ public class ShipController : MonoBehaviour
         this.gameObject.transform.Translate(Speed, 0, 0);
     }
 
-    // OnControllerColliderHit is called when the controller hits a collider while performing a Move
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Debug.Log($"ShipController OnControllerColliderHit");
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log($"ShipController is triggered and melting");
+        Debug.Log($"{this.gameObject.name} is triggered and melting");
         if (collision.rigidbody.gameObject.CompareTag("Torpedo"))
         {
             var contactPoint = collision.contacts[0];
             var explosionPosition = new Vector3(contactPoint.point.x, 0, contactPoint.point.z);
             var explosion = Instantiate(ExplosionPrefab, explosionPosition, Quaternion.LookRotation(Vector3.up));
+            _explosions.Add(explosion);
             explosion.transform.parent = this.gameObject.transform;
             AudioSource.PlayClipAtPoint(ExplosionEffect,  explosionPosition);
+            _hitCount++;
+            if (_hitCount == MaxHits)
+            {
+                var sinkAnimator = this.GetComponentInChildren<Animator>();
+                if (sinkAnimator != null)
+                {
+                    var length = sinkAnimator.runtimeAnimatorController.animationClips[0].length;
+                    Debug.Log($"{this.gameObject.name} sinking animation is {length} sec");
+                    sinkAnimator.enabled = true;
+                    StartCoroutine(DestroySunkenShip(length - 0.5f));
+                }
+                else
+                {
+                    StartCoroutine(DestroySunkenShip(0.5f));
+                }
+            }
         }
+    }
+
+    private IEnumerator DestroySunkenShip(float delay)
+    {
+        yield return new WaitForSeconds(delay/2);
+        foreach (var explosion in _explosions)
+        {
+            Object.Destroy(explosion.gameObject);
+        }
+        yield return new WaitForSeconds(delay/2);
+        Object.Destroy(this.gameObject);
     }
 
 }
